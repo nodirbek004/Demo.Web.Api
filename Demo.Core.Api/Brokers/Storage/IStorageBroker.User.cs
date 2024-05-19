838a140c078b738262b2dea11233adf0a8ae7161
samsung <samsung@DESKTOP-LGSDQV0>
2024-05-19 18:00:38 +0500

    BROKER: Storage Broker

--- Demo.Core.Api/Brokers/Storage/IStorageBroker.User.cs
+++ Demo.Core.Api/Brokers/Storage/IStorageBroker.User.cs
@@ -3,13 +3,15 @@
 // FREE TO USE FOR THE WORLD
 // -------------------------------------------------------
 
-using Demo.Core.Api.Models.Users;
-using System.Linq;
 
 namespace Demo.Core.Api.Brokers.Storage
 {
     public partial interface IStorageBroker
     {
-
+        ValueTask<User> InserUserAsync(User user);
+        IQueriable<User> SelectAllUser();
+        ValueTask<User> SelectUserByIdAsync(int id);
+        ValueTask<User> UpdateUserAsync(User user);
+        ValueTask<User> DeleteUserAsync(User user);
     }
 }
---
+++ Demo.Core.Api/Brokers/Storage/StorageBroker.User.cs
@@ -0,0 +1,12 @@
+﻿using Demo.Core.Api.Models.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Core.Api.Brokers.Storage
+{
+    public partial class StorageBroker
+    {
+        DbSet<User> Users { get; set; }
+        
+
+    }
+}
